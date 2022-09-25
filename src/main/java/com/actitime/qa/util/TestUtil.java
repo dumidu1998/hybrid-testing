67a9689b49fc0600401292fbc51987f21c856445
Dumidu Kasun Bandara Rajakaruna <50117273+dumidu1998@users.noreply.github.com>
2022-09-25 16:55:49 +0530

    IMPLICIT_WAIT to 20 s

--- src/main/java/com/actitime/qa/util/TestUtil.java
+++ src/main/java/com/actitime/qa/util/TestUtil.java
@@ -21,7 +21,7 @@ import org.apache.commons.io.FileUtils;
 public class TestUtil extends TestBase {
 
 	public static long PAGE_LOAD_TIMEOUT =40;
-	public static long IMPLICIT_WAIT = 40;
+	public static long IMPLICIT_WAIT = 20;
 	
 	
 	public void switchToFrame(String framaName) {
